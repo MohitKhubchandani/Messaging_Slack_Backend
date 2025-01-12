72772acf23d24c2b3c1b31b3d82ad5e61b7f9468
Mohit04 <mohitkhubchandani88@gmail.com>
2025-01-12 22:39:53 +0530

    Added code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
+import {BullMQAdapter} from '@bull-board/api/bullMQAdapter';
+import { ExpressAdapter } from '@bull-board/express';
+import { createBullBoard } from 'bull-board';
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
 
 import connectDB from './config/dbConfig.js';
 import { PORT } from './config/serverConfig.js';
+import mailQueue from './queues/mailQueue.js';
 import apiRouter from './routes/apiRoutes.js';
 const app = express();
+
+
 
 app.use(express.json());
 app.use(express.urlencoded());
 
+const bullServerAdapter = new ExpressAdapter()
+
+createBullBoard({
+  queues: [new BullMQAdapter(mailQueue)],
+  bullServerAdapter
+})
+
+app.use('./ui', bullServerAdapter.getRouter());
+ 
 app.use('/api', apiRouter);
 app.use('/ping', (req, res) => {
   return res.status(StatusCodes.OK).json({ message: 'Pong' });
