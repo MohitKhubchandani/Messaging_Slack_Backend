2cc89fbd504e68cb49d3c8995b378a384faf6f59
Mohit04 <mohitkhubchandani88@gmail.com>
2025-01-19 14:35:16 +0530

    Added redis board using bull-board

--- src/config/bullBoardConfig.js
+++ src/config/bullBoardConfig.js
@@ -3,13 +3,14 @@ import { BullAdapter } from '@bull-board/api/bullAdapter.js';
 import { ExpressAdapter } from '@bull-board/express';
 
 import mailQueue from '../queues/mailQueue.js';
+import testQueue from '../queues/testQueue.js';
 
 
 const bullServerAdapter = new ExpressAdapter()
 bullServerAdapter.setBasePath('/ui')
 
 createBullBoard({
-  queues: [new BullAdapter(mailQueue), new BullAdapter(mailQueue)],
+  queues: [new BullAdapter(mailQueue), new BullAdapter(testQueue)],
   serverAdapter : bullServerAdapter
 })
 
