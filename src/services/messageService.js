7997a36014a98c06d6222bda7b6004852d0ee329
Mohit04 <mohitkhubchandani88@gmail.com>
2025-01-27 00:10:59 +0530

    Added code

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -1,6 +1,14 @@
-import messageRepository from "../repositories/messageRepository"
+import channelRepository from "../repositories/channelRepository.js"
+import messageRepository from "../repositories/messageRepository.js"
 
 export const getMessagesService = async (messageParams, page, limit) => {
+    
+    const channelDetails = await channelRepository.getChannelWithWorkspaceDetails(messageParams.channelId);
+
+    const workspace = channelDetails.workspace
+
+    const isMember = await isMe
+
     const messages = await messageRepository.getPaginatedMessages(
     messageParams, 
     page,
